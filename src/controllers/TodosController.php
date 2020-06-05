de8dab4f81f4935e2b57d31112f6087b3fe8c990
Yonathan Román <yonathanr11.12@gmail.com>
2020-06-04 22:41:12 -0500

    Se agrega metodos de Controller

--- src/controllers/TodosController.php
+++ src/controllers/TodosController.php
@@ -4,5 +4,33 @@ use App\Models\TodosModel;
 
 class TodosController
 {
-    
+    // * Mostrar todos los registros con valor del 'status' y idUsuario
+    public static function getAllByStatusAndUsuario(int $status, int $todo)
+    {
+        $todo = TodosModel::all()->where('status', $status)->where('idtodos', $todo);
+        return $todo;
+    }
+
+    // * Crear un registro nuevo
+    public static function postTodo(array $data)
+    {
+        $todo = TodosModel::create($data);
+        return $todo;
+    }
+
+    // * Actualizar un registro
+    public static function putTodo(array $data)
+    {
+        $todo = TodosModel::find($data['id']);
+        $todo->update($data);
+        return $todo;
+    }
+
+    // * Borrado logico de un registro
+    public static function deleteTodo(array $data)
+    {
+        $todo = TodosModel::find($data['id']);
+        $todo->update($data);
+        return $todo;
+    }
 }
--- src/controllers/UsuariosController.php
+++ src/controllers/UsuariosController.php
@@ -4,5 +4,11 @@ use App\Models\UsuariosModel;
 
 class UsuariosController
 {
-    
+    // * Actualizar un registro
+    public static function putTodo(array $data)
+    {
+        $todo = UsuariosModel::find($data['idUser']);
+        $todo->update($data);
+        return $todo;
+    }
 }
