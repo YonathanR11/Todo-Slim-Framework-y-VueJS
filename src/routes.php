337256813686814232c0fd6669be8dd75a1e4783
Yonathan Román <yonathanr11.12@gmail.com>
2020-06-04 22:43:15 -0500

    Se agregan rutas de API

--- src/routes.php
+++ src/routes.php
@@ -9,36 +9,58 @@ return function (App $app) {
     $container = $app->getContainer();
 
     $app->post('/login', function (Request $request, Response $response, array $args) {
-
         $input = $request->getParsedBody();
-        $sql = "SELECT * FROM users WHERE email= :email";
+        $sql = "SELECT * FROM usuarios WHERE usuario= :usuario";
         $sth = $this->db->prepare($sql);
-        $sth->bindParam("email", $input['email']);
+        $sth->bindParam("usuario", $input['usuario']);
         $sth->execute();
         $user = $sth->fetchObject();
-
         // verify email address.
         if (!$user) {
             return $this->response->withJson(['error' => true, 'message' => 'Usuario o contraseña incorrecta.']);
         }
-
         // verify password.
         if (!password_verify($input['password'], $user->password)) {
             return $this->response->withJson(['error' => true, 'message' => 'Usuario o contraseña incorrecta.']);
         }
-
         $settings = $this->get('settings'); // get settings array.
-
-        $token = JWT::encode(['id' => intval($user->id), 'email' => $user->email], $settings['jwt']['secret'], "HS256");
-
+        $token = JWT::encode(['id' => intval($user->idUser), 'usuario' => $user->usuario], $settings['jwt']['secret'], "HS256");
         return $this->response->withJson(['error' => false, 'token' => $token]);
     });
 
     $app->group('/api', function (\Slim\App $app) {
 
-        // ? Obtener datos del token
-        // $app->get('/user', function (Request $request, Response $response, array $args) {
-        //     print_r($request->getAttribute('token_data'));
-        // });
+        // ? Obtener todos los todos de un usuario y status
+        $app->get('/todo/{status}/{idUser}', function (Request $request, Response $response, array $args) {
+            $res = TodosController::getAllByStatusAndUsuario($args['status'], $args['idUser']);
+            $response = $response->withHeader("Content-Type", "application/json")
+                ->withStatus(200)
+                ->withJson($res);
+            return $response;
+        });
+        // ? Crear todo
+        $app->post('/todo', function (Request $request, Response $response) {
+            $res = TodosController::postTodo($request->getParsedBody());
+            $response = $response->withHeader("Content-Type", "application/json")
+                ->withStatus(200)
+                ->withJson($res);
+            return $response;
+        });
+        // ? Actualizar usuario
+        $app->put('/todo', function (Request $request, Response $response) {
+            $res = TodosController::putTodo($request->getParsedBody());
+            $response = $response->withHeader("Content-Type", "application/json")
+                ->withStatus(200)
+                ->withJson($res);
+            return $response;
+        });
+        // ? Eliminar un registro de forma logica
+        $app->delete('/user', function (Request $request, Response $response) {
+            $res = TodosController::deleteTodo($request->getParsedBody());
+            $response = $response->withHeader("Content-Type", "application/json")
+                ->withStatus(200)
+                ->withJson($res);
+            return $response;
+        });
     });
 };
